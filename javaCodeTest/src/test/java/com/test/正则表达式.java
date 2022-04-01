a361ebe6b99dd1cb758d2f687c69ad36252ec659
shuxuecode <zhaoshuxue@163.com>
2022-04-01 20:34:23 +0800

    commitByZsx

---
+++ javaCodeTest/src/test/java/com/test/正则表达式.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+/**
+ * @date 2022/4/1
+ */
+public class 正则表达式 {
+
+    @Test
+    void t1() {
+        Pattern pattern = Pattern.compile("testName.*");
+        boolean matches = pattern.matcher("testName$hidden").matches();
+        Assertions.assertTrue(matches, "正则匹配失败");
+    }
+}
