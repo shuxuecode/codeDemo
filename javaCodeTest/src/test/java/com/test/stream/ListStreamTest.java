8a69480628f17d5b5872de2be15474cf34598386
shuxuecode <zhaoshuxue@163.com>
2022-05-13 21:59:47 +0800

    commitByZsx

---
+++ javaCodeTest/src/test/java/com/test/stream/ListStreamTest.java
@@ -0,0 +1,28 @@
+package com.test.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @date 2022/5/13
+ */
+public class ListStreamTest {
+
+
+    @Test
+    void t1(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        List<Integer> res = list.stream().filter(item -> item > 2).collect(Collectors.toList());
+        System.out.println(res);
+    }
+
+}
