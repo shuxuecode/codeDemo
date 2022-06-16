829ec40d26ad1dd317586777283c3ed4f210e48b
shuxuecode <zhaoshuxue@163.com>
2022-06-16 22:28:07 +0800

    commitByZsx

--- toolTest/src/test/java/UnitTest.java
+++ toolTest/src/test/java/UnitTest.java
@@ -1,4 +1,8 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 /**
  * @author
@@ -8,6 +12,33 @@ public class UnitTest {
 
     @Test
     void t1() {
+        Demo demo = new Demo();
+        demo.setNum(123);
 
+        String str1 = JSON.toJSONString(demo);
+        System.out.println(str1);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("num", "222");
+
+        String str2 = JSON.toJSONString(map);
+        System.out.println(str2);
+
+        Demo demo1 = JSONObject.parseObject(str2, Demo.class);
+        String str3 = JSON.toJSONString(demo1);
+        System.out.println(str3);
+
+    }
+
+    static class Demo {
+        private int num;
+
+        public int getNum() {
+            return num;
+        }
+
+        public void setNum(int num) {
+            this.num = num;
+        }
     }
 }
