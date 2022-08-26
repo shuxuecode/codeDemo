fee458c99ad167967ac894e7e0e76c3e8376aaea
shuxuecode <zhaoshuxue@163.com>
2022-08-26 18:31:37 +0800

    :truck: :chart_with_upwards_trend: :triangular_flag_on_post:

--- javaCodeTest/src/test/java/com/test/datetime/DateTest.java
+++ javaCodeTest/src/test/java/com/test/datetime/DateTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author
@@ -21,6 +22,24 @@ public class DateTest {
         Date date2 = dateFormat.parse("2022-05-20 12:12:13");
 
         System.out.println(date1.after(date2));
+
+    }
+
+    @Test
+    void t2() throws InterruptedException {
+        Date date1 = new Date();
+        System.out.println(date1.compareTo(date1));
+        System.out.println(date1.compareTo(date1) == 0); // true
+
+        TimeUnit.MILLISECONDS.sleep(5);
+
+        Date date2 = new Date();
+        System.out.println(date1.compareTo(date2)); // -1
+        System.out.println(date1.compareTo(date2) < 0); // true
+
+
+        System.out.println(date2.compareTo(date1)); // 1
+        System.out.println(date2.compareTo(date1) > 0); // true
 
     }
 }
---
+++ toolTest/src/main/java/com/zsx/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.zsx.demo;
+
+import lombok.Data;
+
+/**
+ * @date 2022/8/26
+ */
+@Data
+public class Demo {
+}
