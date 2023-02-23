f30dbb78a4428cd6571143d9f040c83605ed5ca1
shuxuecode <zhaoshuxue@163.com>
2023-02-23 20:23:29 +0800

    :art: :lock: :globe_with_meridians:

--- toolTest/groovyTest/src/test/groovy/Test01.groovy
+++ toolTest/groovyTest/src/test/groovy/Test01.groovy
@@ -1,4 +1,19 @@
+package groovy
+
+import com.google.common.collect.Lists;
+
+/**
+ * 静态变量
+ */
+class Globals {
+    static String str = "abc";
+}
+
 
 def Test01() {
+
+    def list = Lists.newArrayList("1", "2", "3")
+
+    return list;
 
 }
--- toolTest/groovyTest/src/test/java/groovy/GroovyScriptTest.java
+++ toolTest/groovyTest/src/test/java/groovy/GroovyScriptTest.java
@@ -4,6 +4,8 @@ import groovy.lang.GroovyShell;
 import groovy.lang.Script;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 /**
  * @date 2023/2/23
  */
@@ -20,6 +22,35 @@ public class GroovyScriptTest {
         Object res = script.invokeMethod("Hello", null);
     }
 
+    @Test void t2(){
+
+        GroovyShell shell = new GroovyShell();
+
+        Script script = shell.parse("package groovy\n" +
+                "\n" +
+                "import com.google.common.collect.Lists;\n" +
+                "\n" +
+                "/**\n" +
+                " * 静态变量\n" +
+                " */\n" +
+                "class Globals {\n" +
+                "    static String str = \"abc\";\n" +
+                "}\n" +
+                "\n" +
+                "\n" +
+                "def Test01() {\n" +
+                "\n" +
+                "    def list = Lists.newArrayList(\"1\", \"2\", \"3\")\n" +
+                "\n" +
+                "    return list;\n" +
+                "\n" +
+                "}"
+        );
+
+        Object res = script.invokeMethod("Test01", null);
+        System.out.println(res instanceof ArrayList);
+        System.out.println(res);
+    }
 
 
 
