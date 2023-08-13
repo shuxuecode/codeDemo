12683bca3f4701eddf3a9445bd6e3190df1da61b
zhaoshuxue <zhaoshuxue@163.com>
2023-08-13 22:11:52 +0800

    :arrow_up: :construction: :fire:

---
+++ toolTest/kryo-test/src/test/java/FuryTest.java
@@ -0,0 +1,53 @@
+import com.zsx.fury.vo.Demo;
+import io.fury.Fury;
+import io.fury.Language;
+import io.fury.ThreadSafeFury;
+import io.fury.serializer.CompatibleMode;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.util.Date;
+
+public class FuryTest {
+
+
+    @Test
+    void t1() {
+        Demo demo = new Demo();
+        demo.setId(1);
+        demo.setName("test");
+        demo.setBirthday(new Date());
+
+
+        Fury fury = Fury.builder().withLanguage(Language.JAVA).build();
+        fury.register(Demo.class);
+
+        byte[] bytes = fury.serialize(demo);
+
+        System.out.println(fury.deserialize(bytes));
+
+
+    }
+
+
+    @Test
+    void t2() {
+        // 线程安全
+        ThreadSafeFury fury = Fury.builder()
+                .withLanguage(Language.JAVA)
+                .withRefTracking(false)
+                .withCompatibleMode(CompatibleMode.SCHEMA_CONSISTENT)
+                .withAsyncCompilation(true)
+                .buildThreadSafeFury();
+
+        fury.getCurrentFury().register(Demo.class);
+
+        Demo demo = new Demo(1, "123", new Date());
+
+        byte[] bytes = fury.serialize(demo);
+
+        System.out.println(fury.deserialize(bytes));
+
+    }
+
+}
