97e107ee80d94cbb3e1891560f875e63016b0f97
shuxuecode <zhaoshuxue@163.com>
2023-09-03 19:51:35 +0800

    :wheelchair: :rewind: :boom:

--- toolTest/common/src/test/java/jsonpath/JsonPathUnitTest.java
+++ toolTest/common/src/test/java/jsonpath/JsonPathUnitTest.java
@@ -1,10 +1,13 @@
 package jsonpath;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
 import com.jayway.jsonpath.JsonPath;
 import groovy.util.GroovyTestCase;
 
+import java.util.Collection;
 import java.util.Date;
 
 /**
@@ -29,5 +32,39 @@ public class JsonPathUnitTest extends GroovyTestCase {
 
         Object read = JsonPath.read(JSON.toJSONString(str), "$.id");
         System.out.println(read);
+    }
+
+    public void test03() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("a", 1);
+        jsonObject.put("b", 2);
+        jsonObject.put("c", 3);
+
+        Collection<String> set = (Collection<String>) JSONPath.eval(jsonObject, "$.keySet()");
+
+        set.stream().forEach(System.out::println);
+
+    }
+
+    public void test04() {
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("a", 1);
+
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("a", 2);
+
+        JSONObject jsonObject3 = new JSONObject();
+        jsonObject3.put("a", 3);
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(jsonObject1);
+        jsonArray.add(jsonObject2);
+        jsonArray.add(jsonObject3);
+
+        System.out.println(jsonArray.toJSONString());
+
+        System.out.println(JSONPath.eval(jsonArray, "$[1]"));
+        System.out.println(JSONPath.eval(jsonArray, "$[1].a"));
+        System.out.println(JSONPath.eval(jsonArray, "$[1].b"));
     }
 }
