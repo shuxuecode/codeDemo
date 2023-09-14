d9f7c208a2fdec191b25ddf9e3c1e305dd23bff4
zhaoshuxue <zhaoshuxue@163.com>
2023-09-14 22:35:32 +0800

    :construction: :white_check_mark: :bricks:

---
+++ toolTest/common/src/main/java/com/zsx/core/TT.java
@@ -0,0 +1,24 @@
+package com.zsx.core;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+/**
+ * todo mark
+ */
+public class TT implements ClassFileTransformer {
+
+    @Override
+    public byte[] transform(
+            ClassLoader loader,
+            String className,
+            Class<?> classBeingRedefined,
+            ProtectionDomain protectionDomain,
+            byte[] classfileBuffer) throws IllegalClassFormatException {
+
+
+        return classfileBuffer;
+    }
+}
