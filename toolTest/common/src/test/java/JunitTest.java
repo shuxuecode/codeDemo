f9b15a97140ad62feed347c90c9b4a275396760b
zhaoshuxue <zhaoshuxue@163.com>
2023-09-14 22:42:51 +0800

    :arrow_down: :pushpin: :monocle_face:

--- toolTest/common/src/test/java/JunitTest.java
+++ toolTest/common/src/test/java/JunitTest.java
@@ -1,2 +1,18 @@
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
 public class JunitTest {
+
+
+    public static void main(String[] args) {
+        TestRunner.run(T.class);
+    }
+
+    public class T extends TestCase {
+
+        {
+            System.out.println("123");
+        }
+    }
 }
+
