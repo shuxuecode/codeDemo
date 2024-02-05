b58e1d0f169fc16fc2a26bb0fa999f27f9ec8688
shuxuecode <zhaoshuxue@163.com>
2024-02-05 22:59:54 +0800

    :boom: :package: :mute:

--- javaCodeTest/src/test/java/com/test/demo/Demo.java
+++ javaCodeTest/src/test/java/com/test/demo/Demo.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ *
  */
 public class Demo {
 
@@ -25,7 +27,61 @@ public class Demo {
     }
 
 
+    @Test
+    void t2() {
+        // todo zsx
 
+        // Comparison method violates its general contract
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(1);
+        list.add(2);
+
+        AtomicInteger i = new AtomicInteger();
+
+        list.sort((o1, o2) -> {
+
+            i.getAndIncrement();
+
+            System.out.println("== " + i);
+            System.out.println(o2);
+            System.out.println(o1);
+
+            if (o1 == 1 && o2 == 3) {
+                return -1;
+            }
+
+            if (o1 == 2 && o2 == 1) {
+                if (i.get() == 4) {
+                    return 0;
+                }
+                return 1;
+            }
+
+            if (o1 == 2 && o2 == 3) {
+                return -1;
+            }
+
+            try {
+                // Integer a = Integer.valueOf(map.get(o1));
+                // Integer b = Integer.valueOf(map.get(o2));
+                // if (a.equals(b)) {
+                //     return 0;
+                // } else if (a.compareTo(b) > 0) {
+                //     return 1;
+                // } else {
+                //     return -1;
+                // }
+                return 0;
+            } catch (Exception e) {
+                return 1;
+            }
+        });
+
+        System.out.println(list);
+
+    }
 
 
 }
