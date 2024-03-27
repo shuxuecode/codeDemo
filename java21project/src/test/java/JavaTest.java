00fc68d0e3aa6291e3d9912656419bced5000ad1
shuxuecode <zhaoshuxue@163.com>
2024-03-27 20:30:01 +0800

    :clown_face: :alien: :rotating_light:

--- java21project/src/test/java/JavaTest.java
+++ java21project/src/test/java/JavaTest.java
@@ -1,18 +1,48 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class JavaTest {
-
-
-
 
 
     @Test
     public void t() {
 
+        // 创建虚拟线程
+        try (ExecutorService taskExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
+
+            for (int i = 0; i < 10; i++) {
+                int finalI = i;
+                taskExecutor.submit(() -> {
+                    TimeUnit.MILLISECONDS.sleep(500L);
+                    System.out.println(finalI);
+                    return finalI;
+                });
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
 
 
+    @Test
+    void t2() {
+        // 创建虚拟线程，默认不启动
+        Thread thread = Thread.ofVirtual().name("test").unstarted(() -> {
+            System.out.println("test");
+        });
+
+        thread.start();
+
+        // 创建虚拟线程
+        Thread.ofVirtual().name("test2").start(() -> {
+            System.out.println("test2");
+        });
+    }
 
 }
