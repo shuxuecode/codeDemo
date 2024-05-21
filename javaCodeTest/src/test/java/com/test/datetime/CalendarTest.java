87a615622141454726448753210d1b82121c1a26
shuxuecode <zhaoshuxue@163.com>
2024-05-21 21:10:58 +0800

    :truck: :label: :passport_control:

--- javaCodeTest/src/test/java/com/test/datetime/CalendarTest.java
+++ javaCodeTest/src/test/java/com/test/datetime/CalendarTest.java
@@ -2,6 +2,8 @@ package com.test.datetime;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -16,6 +18,27 @@ public class CalendarTest {
         calendar.add(Calendar.DAY_OF_MONTH, -1);
 
         System.out.println(calendar.getTime());
+
+    }
+
+
+
+    @Test void t2(){
+        Calendar calendar = Calendar.getInstance();
+
+        System.out.println(calendar.getTime());
+        System.out.println(calendar.toInstant().atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
+        System.out.println(calendar.get(Calendar.MILLISECOND));
+
+
+
+        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        // calendar.set(Calendar.MILLISECOND, 0);
+
+        System.out.println(calendar.getTime());
+        System.out.println(calendar.toInstant().atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
 
     }
 
