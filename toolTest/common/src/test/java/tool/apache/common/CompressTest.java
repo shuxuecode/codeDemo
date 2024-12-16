dd6d8c5cc874ca991ca2830d9c1e17554c342c54
shuxuecode <zhaoshuxue@163.com>
2024-12-16 19:51:36 +0800

    :truck: :fire: :dizzy:

---
+++ toolTest/common/src/test/java/tool/apache/common/CompressTest.java
@@ -0,0 +1,51 @@
+package tool.apache.common;
+
+import groovy.util.GroovyTestCase;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author shuxuecode
+ * @date 2024/12/16
+ */
+public class CompressTest extends GroovyTestCase {
+
+
+    public void test() {
+
+        String userHomePath = System.getProperty("user.home");
+        System.out.println(userHomePath);
+
+        File outputFile = new File(userHomePath + "/tmp/testZip.zip");
+        File inputFile = new File(userHomePath + "/tmp/0.txt");
+
+        try (
+                ZipArchiveOutputStream zipOutput = new ZipArchiveOutputStream(new FileOutputStream(outputFile));
+                FileInputStream fis = new FileInputStream(inputFile)
+        ) {
+
+            ZipArchiveEntry entry = new ZipArchiveEntry(inputFile.getName());
+            zipOutput.putArchiveEntry(entry);
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fis.read(buffer)) != -1) {
+                zipOutput.write(buffer, 0, len);
+            }
+
+            zipOutput.closeArchiveEntry();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
