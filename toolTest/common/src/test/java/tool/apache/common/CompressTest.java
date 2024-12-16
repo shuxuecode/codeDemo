ac440c715d9a448e6d610e11f43184cb4cba93a7
shuxuecode <zhaoshuxue@163.com>
2024-12-17 00:03:27 +0800

    :dizzy: :poop: :coffin:

--- toolTest/common/src/test/java/tool/apache/common/CompressTest.java
+++ toolTest/common/src/test/java/tool/apache/common/CompressTest.java
@@ -4,10 +4,10 @@ import groovy.util.GroovyTestCase;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * @author shuxuecode
@@ -47,5 +47,42 @@ public class CompressTest extends GroovyTestCase {
 
     }
 
+    public void testJieYa() {
+        String userHomePath = System.getProperty("user.home");
+        System.out.println(userHomePath);
+
+        File zipFile = new File(userHomePath + "/tmp/testZip.zip");
+        File outputDir = new File(userHomePath + "/tmp/1");
+
+        if (!outputDir.exists()) {
+            outputDir.mkdir();
+        }
+
+        try (ZipFile zip = new ZipFile(zipFile)) {
+            Enumeration<? extends ZipEntry> entries = zip.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+
+                if (!entry.isDirectory()) {
+                    File outFile = new File(outputDir, entry.getName());
+
+                    try (InputStream is = zip.getInputStream(entry);
+                         FileOutputStream fos = new FileOutputStream(outFile)) {
+
+                        byte[] buffer = new byte[1024];
+                        int len;
+                        while ((len = is.read(buffer)) != -1) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+
+                }
+
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
