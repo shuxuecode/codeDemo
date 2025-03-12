25821d96a3860563af09d7e56b159c969ccebaf0
shuxuecode <zhaoshuxue@163.com>
2025-03-12 11:48:25 +0800

    :pencil2: :lipstick: :dizzy:

--- toolTest/common/src/test/java/MockitoTest.java
+++ toolTest/common/src/test/java/MockitoTest.java
@@ -1,18 +1,33 @@
 import com.zsx.mockito.UserService;
 import com.zsx.mockito.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.mockito.Mockito.*;
 
 public class MockitoTest {
 
+    @Mock
+    UserService userService;
+
+    @InjectMocks
+    UserServiceImpl userServiceImpl;
+
+    @BeforeEach
+    public void setUp() {
+        // 初始化 @Mock 注解标记的对象
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void test01() {
 
         // 创建mock对象
-        UserService userService = mock(UserService.class);
+        //UserService userService = mock(UserService.class);
 
         // 定义预期行为
         when(userService.getUserName("1")).thenReturn("zsx");
