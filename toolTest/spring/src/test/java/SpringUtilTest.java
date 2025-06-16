6a0c0f3f05f730ad5d05e7ba87d84822b1838ba5
shuxuecode <zhaoshuxue@163.com>
2025-06-16 20:49:08 +0800

    :memo: :iphone: :adhesive_bandage:

--- toolTest/spring/src/test/java/SpringUtilTest.java
+++ toolTest/spring/src/test/java/SpringUtilTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.util.DigestUtils;
+import org.springframework.util.StringUtils;
 
 /**
  * @author shuxuecode
@@ -12,6 +13,11 @@ public class SpringUtilTest {
     @Test
     public void test() {
 
+        String abc = StringUtils.capitalize("abc");
+        System.out.println(abc);
+
+        String delete = StringUtils.delete("abc", "a");
+        System.out.println(delete);
 
     }
 
