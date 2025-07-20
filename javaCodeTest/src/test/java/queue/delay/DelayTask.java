45f62989c3ff796d8ff8925c373afb922d621d30
shuxuecode <zhaoshuxue@163.com>
2025-07-20 23:43:17 +0800

    :mute: :heavy_minus_sign: :truck:

--- javaCodeTest/src/test/java/queue/delay/DelayTask.java
+++ javaCodeTest/src/test/java/queue/delay/DelayTask.java
@@ -4,6 +4,17 @@ import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * 1、功能概述：
+ * 封装一个任务（T task）和延迟时间（long time），用于在延迟队列中执行。
+ * 实现了 Delayed 接口，支持任务按延迟时间排序和计算剩余延迟时间。
+ * <p>
+ * 2、关键方法：
+ * compareTo(Delayed o)：比较两个任务的延迟时间，用于排序。
+ * getDelay(TimeUnit unit)：返回任务剩余的延迟时间。
+ * <p>
+ * 3、使用场景：
+ * 适用于需要延迟执行任务的场景，如定时任务调度、消息队列延迟投递等。
+ *
  * @date 2022/7/8
  */
 public class DelayTask<T> implements Delayed {
