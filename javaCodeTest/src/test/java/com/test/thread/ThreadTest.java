cc4a3207daaa5890bc06b1d6bb5fa76b41ddef28
shuxuecode <zhaoshuxue@163.com>
2025-08-08 18:07:35 +0800

    :globe_with_meridians: :rocket: :heavy_minus_sign:

--- javaCodeTest/src/test/java/com/test/thread/ThreadTest.java
+++ javaCodeTest/src/test/java/com/test/thread/ThreadTest.java
@@ -3,20 +3,7 @@ package com.test.thread;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 
 /**
  * @date 2022/6/21
@@ -109,5 +96,72 @@ public class ThreadTest {
         System.out.println(list2);
     }
 
+
+    @Test
+    void t04() throws InterruptedException {
+
+        ExecutorService executorService = new ThreadPoolExecutor(
+                2, 4,
+                10, TimeUnit.SECONDS,
+                queue,
+                new MyThreadFactory((t, e) -> System.out.println("线程" + t.getName() + "异常" + e))
+        );
+
+        //try {
+        //    executorService.execute(() -> {
+        //        try {
+        //            Integer.valueOf("a");
+        //        } catch (Exception e) {
+        //            System.out.println("异常" + e);
+        //            throw e;
+        //        }
+        //    });
+        //} catch (Exception e) {
+        //    System.out.println("外层异常" + e);
+        //}
+
+        try {
+            Future<?> future = executorService.submit(() -> {
+                try {
+                    Integer.valueOf("a");
+                } catch (Exception e) {
+                    System.out.println("异常" + e);
+                    throw e;
+                }
+            });
+            future.get(1L, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            System.out.println("外层异常" + e);
+        }
+
+        TimeUnit.SECONDS.sleep(2L);
+    }
+
+    public static class MyThreadFactory implements ThreadFactory {
+
+
+        private final Thread.UncaughtExceptionHandler exceptionHandler;
+
+        public MyThreadFactory(Thread.UncaughtExceptionHandler exceptionHandler) {
+            this.exceptionHandler = exceptionHandler;
+        }
+
+        /**
+         * Constructs a new {@code Thread}.  Implementations may also initialize
+         * priority, name, daemon status, {@code ThreadGroup}, etc.
+         *
+         * @param r a runnable to be executed by new thread instance
+         * @return constructed thread, or {@code null} if the request to
+         * create a thread is rejected
+         */
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            System.out.println(123);
+            // 设置异常处理器
+            thread.setUncaughtExceptionHandler(exceptionHandler);
+            return thread;
+        }
+    }
 
 }
